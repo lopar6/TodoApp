69042ed489142dbe68d7b0fc889502fa8309aa31
lopar6 <lopar6@gmail.com>
2021-06-19 18:21:18 -0600

    refactoring and update title values feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import reactDom from 'react-dom';
 
 
 // todo check for first time visit and give them a little tour
@@ -14,6 +15,7 @@ class Task extends React.Component{
     }
     // needed to make 'this' work in callback
     this.changePriorityState = this.changePriorityState.bind(this);
+    this.changeTitle = this.changeTitle.bind(this);
   }
 
   // take in Task object, change priority
@@ -28,16 +30,24 @@ class Task extends React.Component{
     if(this.state.priority == 'high'){
       this.setState({priority : 'low'})    
     }
-
-
   } 
+
+  changeTitle(event){
+    //todo change title of task with API call
+    //todo update state of title
+    
+    this.setState({title: event.target.value})
+  }
+
+  moveElement(){
+    // move self to another place on list
+  }
 
   render() {
     return ( 
-      <div className={`task-box ${this.state.priority}-border`} >
-        <div className={"title-display"}>
-          {this.state.title}
-        </div>
+        //todo change the 'onChange' to something that exicutes after user is done for more effdeciency
+      <div className={`task-box ${this.state.priority}-border`} onClick={this.moveElement} >
+        <input type="text" className={"title-display"} onChange={this.changeTitle} value={this.state.title}/>
           <div className="edit-buttons" >
             <PriorityButton size={"small-button"} priority={this.state.priority} click={this.changePriorityState}/>
             <Trashbutton/>
@@ -85,53 +95,55 @@ class Todo extends React.Component{
     super(props)
     let _tempDivs = []
     for(let i=0; i < 5; i++){
-      _tempDivs.push(<Task title={`task ${i}`} priority='low' />)
+      _tempDivs.push(<Task title={`task ${i + 1}`} priority='low' />)
     }
     this.state = {
       //temp for testing
       //replace with api call
       taskDivs: _tempDivs
     } 
+    this.addNewTask = this.addNewTask.bind(this)
   }
   
+  addNewTask(event){
+    // concat returns a new array with whatever was passed in added on
+    let _previousTaskDivs = this.state.taskDivs.concat(<Task title="new task" priority='low'/>)
+    this.setState({taskDivs: _previousTaskDivs})
+    //todo change value with API call
+  }
 
   render(){
     return(
       <div>
-      <NewTodoBox></NewTodoBox>
-      {this.state.taskDivs}
+        <div id="new-todo-box">
+          <form id="new-task-form">
+            <input type="text" id="new-task-title" placeholder="Enter a new task">
+            </input>
+            <div id="priority-buttons">
+                <PriorityButton priority={'low'}/>
+                <input type="button" className={`input-button submit`} value="save" onClick={this.addNewTask}></input>
+            </div>
+          </form>
+        </div>
+        {this.state.taskDivs}
       </div>
     )
   }
   
 }
 
-  // todo make buttons pretty 
+// todo make buttons pretty 
 // todo make buttons only appear when hovering
 function Trashbutton({size = 'small-button', click}){
   return <input type="button" className={`trash ${size}`} onClick={click}></input>
 }
 
-function EditButton({size = 'small-button'}){
-  return <input type="button" className={`edit ${size}`}></input>
-}
-
-// todo add cycle through colors mechanic
-
-function SubmitButton({size = ''}){
-  return <input type="button" className={`input-button submit ${size}`} value="save"></input>
-}
-
-function NewTodoBox(){
-  return <div id="new-todo-box"><NewTaskForm/></div>
-}
-
-function NewTaskForm({children}){
+function NewTaskForm({saveButtonCall}){
   return <form id="new-task-form">
       <input type="text" id="new-task-title" placeholder="Enter a new task"></input>
       <div id="priority-buttons">
           <PriorityButton priority={'low'}/>
-          <SubmitButton/>
+          <input type="button" className={`input-button submit`} value="save" onClick={saveButtonCall}></input>
        </div>
 
   </form>
