3c62c6dde89fc8deed6b9bd8d7918e5be3538701
lopar6 <lopar6@gmail.com>
2021-06-21 16:05:32 -0600

    added remove tasks and add tasks with enter key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import reactDom from 'react-dom';
 
-
+// todo move png to public
 // todo check for first time visit and give them a little tour
 
 class Task extends React.Component{
@@ -13,6 +13,7 @@ class Task extends React.Component{
       title: props.title, 
       priority: props.priority,
       showButtons: null,
+      taskID : props.taskID
     }
 
     // needed to make 'this' work in callback
@@ -21,6 +22,14 @@ class Task extends React.Component{
     this.changeTitle         = this.changeTitle.bind(this);
     this.renderButtons       = this.renderButtons.bind(this);
     this.deRenderButtons     = this.deRenderButtons.bind(this);
+  }
+
+  componentWillReceiveProps({title, taskID, priority}){
+    this.setState({
+      title: title,
+      taskID: taskID,
+      priority: priority,
+    })
   }
 
   // take in Task object, change priority
@@ -56,14 +65,28 @@ class Task extends React.Component{
     setTimeout(() => this.setState({showButtons: false}), 500)
   }
 
+  removeSelf = () => {
+    this.props.removeTask(this.state.taskID)
+  }
+
+  
+
   //todo ask Nate the best structure for deleting tasks
   render() {
     return ( 
-      <div className={`task-box ${this.state.priority}-border`} onClick={this.moveElement} onMouseOver={this.renderButtons} onMouseLeave={this.deRenderButtons}>
-        <input type="text" className={"title-display white-font"} onChange={this.changeTitle} value={this.state.title}/>
+      <div className={`task-box ${this.state.priority}-border`} 
+           onClick={this.moveElement} 
+           onMouseOver={this.renderButtons} 
+           onMouseLeave={this.deRenderButtons}>
+        <input type="text" 
+               className={"title-display white-font"} 
+               onChange={this.changeTitle} 
+               value={this.state.title}/>
           {this.state.showButtons ? <div className="edit-buttons" >
-            <Trashbutton/>
-            <PriorityButton size={"small-button"} priority={this.state.priority} click={this.changePriorityState}/>
+            <Trashbutton removeTask={this.removeSelf}/>
+            <PriorityButton size={"small-button"} 
+                            priority={this.state.priority} 
+                            click={this.changePriorityState}/>
           </div>: null}
       </div>
     )
@@ -75,7 +98,7 @@ class Todo extends React.Component{
     super(props)
     let _tempDivs = []
     for(let i=0; i < 5; i++){
-      _tempDivs.push(<Task title={`task ${i + 1}`} priority='low' />)
+      _tempDivs.push(<Task title={`task ${i + 1}`} priority='low' taskID={i} removeTask={this.removeTask}/>)
     }
     this.state = {
       //temp for testing
@@ -91,7 +114,10 @@ class Todo extends React.Component{
   addNewTask = (event) => {
     if(this.state.newTaskTitle && this.state.newTaskTitle != ''){
       // concat returns a new array with whatever was passed in added on
-      let _previousTaskDivs = this.state.taskDivs.concat(<Task title={this.state.newTaskTitle} priority={this.state.newTaskPriority}/>)
+      let _previousTaskDivs = this.state.taskDivs.concat(<Task title={this.state.newTaskTitle} 
+                                                               priority={this.state.newTaskPriority} 
+                                                               taskID={this.state.taskDivs.length}
+                                                               removeTask={this.removeTask}/>)
       // why wont this work
       // let _previousTaskDivs = [<Task title={this.state.newTaskTitle} priority={this.state.newTaskPriority}/>].concat(this.state.taskDivs)
       this.setState({
@@ -103,10 +129,17 @@ class Todo extends React.Component{
     }
   }
 
-  removeTask = () => {
-    let _tempTaskDivs = this.state.taskDivs;
-    // _tempTaskDivs.
-    // this.setState(taskDivs:)
+  removeTask = (taskID) => {
+    let _tempTaskDivs = this.state.taskDivs.concat()
+    _tempTaskDivs.splice(taskID, 1)
+    // need to update child taskID with new location in array
+    // start from where we spliced
+    for(let i = taskID; i <  _tempTaskDivs.length; i++){
+      // update taskID values to reflect new location in array
+      
+      _tempTaskDivs[i] = React.cloneElement( _tempTaskDivs[i], {taskID: i})
+    }
+    this.setState({taskDivs: _tempTaskDivs})
   }
   
   changeNewTaskTitle = (event) => {
@@ -124,17 +157,33 @@ class Todo extends React.Component{
       this.setState({newTaskPriority : 'low'})    
     }
   }
+
+  handleSubmission = (event) => {
+    event.preventDefault()
+    this.addNewTask(event)
+  }
   
   render(){
     return(
       <div>
         <div id="new-todo-box">
-          <form id="new-task-form">
-            <input type="text" id="new-task-title" placeholder="Enter a new task" onChange={this.changeNewTaskTitle} value={this.state.newTaskTitle}>
+          <form id="new-task-form"
+                onSubmit={this.handleSubmission}>
+            <input type="text" 
+                   id="new-task-title" 
+                   placeholder="Enter a new task" 
+                   onChange={this.changeNewTaskTitle} 
+                   value={this.state.newTaskTitle} 
+                   removeTask={this.removeTask}>
             </input>
             <div id="priority-buttons">
-                <PriorityButton priority={this.state.newTaskPriority} click={this.changeNewTaskPriority}/>
-                <input type="button" className={`input-button submit`} value="save" onClick={this.addNewTask}></input>
+                <PriorityButton priority={this.state.newTaskPriority} 
+                                click={this.changeNewTaskPriority}/>
+                <input type="button" 
+                       className={`input-button submit`} 
+                       value="save" 
+                       onClick={this.addNewTask}>
+                </input>
             </div>
           </form>
         </div>
@@ -154,8 +203,8 @@ function PriorityButton(props){
     )
 }
 
-function Trashbutton({size = 'small-button', click}){
-  return <input type="button" className={`trash ${size}`} onClick={click}></input>
+function Trashbutton({size = 'small-button', removeTask}){
+  return <input type="button" className={`trash ${size}`} onClick={removeTask}></input>
 }
 
 function NewTaskForm({saveButtonCall}){
