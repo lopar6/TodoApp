b9859dc05609d46522d921fa8e119be80669ff9e
lopar6 <lopar6@gmail.com>
2021-06-30 14:21:09 -0600

    bugfix handlerID

--- src/components/task.js
+++ src/components/task.js
@@ -5,75 +5,76 @@ import { Trashbutton } from './trash-button';
 import { PriorityButton } from './priority-button';
 import { cyclePriority } from '../services/cycle-priority';
 
-export function Task({key, index, title, priority, removeTask, moveTask, updateTitle, setPriority}) {
+export function Task({index, title, priority, removeTask, moveTask, updateTitle, setPriority}) {
     const [showButtons, setButtonShow] = useState(null)  
     const ref = useRef(null)
     
     useEffect(() => {
-      console.log(index, title)
-    }, [index, title])
+    }, [index, title, priority])
 
     const [{isDragging}, drag] = useDrag(({
       type: 'Task',
       item: () => {
         return {index}
       },
-      collect: (monitor) => ({
-        isDragging: !!monitor.isDragging(),
-        // todo change this to only passing index
-        taskItem: monitor.getItem()
-      }),
+      collect: (monitor) => {
+
+        return {
+          isDragging: !!monitor.isDragging(),
+          // todo change this to only passing index
+        }
+      },
       dropEffect: "move",
     }))
   // !may need to pass in key value for dnd
 
-    const [, drop, monitor] = useDrop(
-      () => ({
-        accept: 'Task',
-        collect: (monitor) => ({
-          // isDragging: !!monitor.isDragging(),
-          taskItem: monitor.getItem()
-        }),
-
-        //todo add hover moving mechanics
-        hover: (item) => {
-          // const dragKey = item.index
-          // const dropKey = index
-        },
-        drop: (item) => {
-          // Determine rectangle on screen
-          // if the reference to the drop DOM object does not exist
-          // do not continue
-          // let dragTask = item
-          const dragIndex = item.index
-          console.log(item)
-          const dropIndex = index
-          console.log("drag", dragIndex, "drop", dropIndex)
-          // dont replace items with themselves
-          if (dragIndex === dropIndex) {return}
-          moveTask(dragIndex, dropIndex)
-          //! how is this possible
-          item.index = dropIndex
-
-        //todo finish this logic
-
-        if (!ref.current) {return}
-        // find rectangle location of div
-        // const boundingRect = dropRef.current.getBoundingClientRect()
-        // const middleY = (boundingRect.bottom - boundingRect.top) / 2
-        // get mouse position
-        // const mouseOffest = monitor.getClientOffset()
-
-
-        //todo add logic to make sure tasks are moved to the correct place (above or below halfway)
+    const [{handlerId}, drop] = useDrop ({
+      accept: 'Task',
+      collect: (monitor) => {
+        // isDragging: !!monitor.isDragging(),
+        return {
+          handlerId: monitor.getHandlerId()
         }
-      })
-    )
+      },
+
+      //todo add hover moving mechanics
+      hover: (item) => {
+        // const dragKey = item.index
+        // const dropKey = index
+      },
+      drop: (item) => {
+        // Determine rectangle on screen
+        // if the reference to the drop DOM object does not exist
+        // do not continue
+        // let dragTask = item
+        const dragIndex = item.index
+        console.log(item)
+        const dropIndex = index
+        console.log("drag", dragIndex, "drop", dropIndex)
+        // dont replace items with themselves
+        if (dragIndex === dropIndex) {return}
+        moveTask(dragIndex, dropIndex)
+        //! how is this possible
+        // item.index = dropIndex
+
+      //todo finish this logic
+
+      if (!ref.current) {return}
+      // find rectangle location of div
+      // const boundingRect = dropRef.current.getBoundingClientRect()
+      // const middleY = (boundingRect.bottom - boundingRect.top) / 2
+      // get mouse position
+      // const mouseOffest = monitor.getClientOffset()
+
+
+      //todo add logic to make sure tasks are moved to the correct place (above or below halfway)
+      }
+    })
 //  !wtf is this
 //todo make hover move things and drop write changes to DB
     drag(drop(ref))
     return ( 
-      <div ref={ref}>
+      <div ref={ref} data-handler-id={handlerId}>
         {/* {console.log(title, props.index, "has rendered")} */}
         <div className={`task-box ${priority}-border`}
           // onClick={this.moveElement} 
