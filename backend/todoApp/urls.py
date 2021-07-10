e870cce4f41fb8cb0848014c7abff55e2d61da9c
lopar6 <lopar6@gmail.com>
2021-07-10 10:56:45 -0600

    Added viewsets

--- backend/todoApp/urls.py
+++ backend/todoApp/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.urls import include
+from django.conf.urls import url
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
 
-from todoApp import views
+from todoApp.views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename=r'tasks')
 
 urlpatterns = [
-    path('tasks/', views.task_list),
-    path('tasks/<int:pk>/', views.task_detail)
+    # path('tasks/', views.task_list),
+    # path('tasks/<int:pk>/', views.task_detail),
+    url(r'', include(router.urls)),
+    path(r'admin/', admin.site.urls),
 ]
--- backend/todoApp/views.py
+++ backend/todoApp/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, status, mixins
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
 
 from todoApp.models import Task
 from todoApp.serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk):
+        task = Task.objects.filter(id=pk).first()
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+
+    # todo add delete
+
+    # example of good pattern
+    @action(detail = True, methods = ['get'])
+    def title(self, request, pk):
+        task = Task.objects.filter(id=pk).first()
+        if task is None:
+            return Response(None, status=status.HTTP_404_NOT_FOUND)
+        return Response(task.title , status=status.HTTP_200_OK)
+
+    @action(detail = False, methods = ['post'])
+    def batch(self, request):
+        ids = []
+        for _task in request.data:
+            if _task.get('pk'):
+                pk = _task.pop('pk')
+                Task.objects.filter(id=pk).update(**_task)
+                ids.append(pk)
+            else:
+                task = Task()
+                task.index = _task.get('index')
+                task.title = _task.get('title')
+                task.priority = _task.get('priority')
+                task.save()
+                ids.append(task.id)
+        tasks = Task.objects.filter(id__in = ids)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+
+    # todo return all user's tasks, not only updated tasks
 
 # ! todo remove this csrf exemption! not secure
 @csrf_exempt
